48cd6ee79824f0d4e01335b7d8e5d9c0f2df6594
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-03-03 18:54:42 -0500

    creacion de la seccion de comentarios y select

---
+++ consultarDatos/00_comentarios.sql
@@ -0,0 +1,32 @@
+/*
+##########
+COMENTAROS
+##########
+
+En SQL, los comentarios son parte de la sintaxis del lenguaje, se utilizan para documentar y explicar
+el código. Hay dos tipos principales de comentarios en SQL
+*/
+
+/*
+Comentario simple
+Comentarios de una línea: Estos comentarios se utilizan para agregar notas breves o aclaraciones en 
+una sola línea de código. Se indican mediante dos guiones seguidos (--) y todo lo que sigue después
+de ellos en la misma línea se considera un comentario y no se ejecuta como parte de la consulta.
+*/
+
+-- Comentario en una sola línea
+
+/*
+Comentarios multilineas
+Estos comentarios permiten incluir explicaciones más extensas o detalladas que abarcan varias líneas
+de código. Se delimitan mediante /* para iniciar el comentario y */ para terminarlo.
+Todo lo que esté entre estos delimitadores se considera un comentario y no se ejecuta
+como parte de la consulta.
+*/
+
+/*
+Este es un comentario de
+varias líneas en SQL.
+Se utiliza para documentar
+secciones extensas de código.
+*/
---
+++ consultarDatos/01_select.sql
@@ -0,0 +1,12 @@
+/*
+SELECT
+*/
+
+-- Obtiene todos los datos de la tabla "users"
+SELECT * FROM users;
+
+-- Obtiene todos los nombres de la tabla "users"
+SELECT nombres FROM users;
+
+-- Obtiene todos los nombres y apellidos de la tabla "users"
+SELECT nombres, apellidos FROM users;
