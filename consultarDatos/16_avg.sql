9066938a54feda7427e6aea425cdbd85d92b7829
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-03-08 15:34:53 -0500

    creacion de AVG

---
+++ consultarDatos/16_avg.sql
@@ -0,0 +1,8 @@
+/*
+AVG
+
+AVG es una función de agregación en SQL que se utiliza para calcular la media (promedio) de los valores en una columna numérica
+*/
+
+-- Obtiene la media de edad de la tabla "users"
+Select AVG(edad) FROM users;
