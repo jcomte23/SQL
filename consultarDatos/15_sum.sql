cef0ecaa15ba5495efc5fc94e18e116a7425a081
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-03-08 15:30:40 -0500

    creacion de la opcion de sum

---
+++ consultarDatos/15_sum.sql
@@ -0,0 +1,8 @@
+/*
+SUM
+
+Es una función de agregación en SQL que se utiliza para calcular la suma de los valores en una columna numérica.
+*/
+
+-- Suma todos los valores del campo edad de la tabla "users"
+Select SUM(edad) FROM users;
