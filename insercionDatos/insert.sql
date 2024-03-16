02360cbbcd82a8b45277da6993041d5ce8d0cd6d
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-03-15 20:08:53 -0500

    creacion de la insercion de datos

---
+++ insercionDatos/insert.sql
@@ -0,0 +1,10 @@
+/*
+INSERT
+
+esta sentencia nos premite crear un nuevo registro en la base de datos
+*/
+
+SELECT * FROM users
+
+-- registrar un nuevo usuario
+INSERT INTO users (nombres,apellidos,correo,edad,genero,pais,musica) VALUES ("javier","combita tellez","jcomte23@outlook.com",25,"H","colombia","pop");
